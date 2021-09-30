90fcdac67227bb45abcce60a6f5d4aa9403e2ce1
Sophia Lau <slau2022@gmail.com>
2021-09-30 18:15:12 -0400

    Storyboardframe in

---
+++ SustainableScotty/ViewControllers/CommunitiesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CommunitiesViewController.swift
+//  SustainableScotty
+//
+//  Created by Sophia Lau on 9/30/21.
+//
+
+import UIKit
+
+class CommunitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SustainableScotty/ViewControllers/StartViewController.swift
+++ SustainableScotty/ViewControllers/StartViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartViewController.swift
 //  SustainableScotty
 //
 //  Created by Sophia Lau on 9/30/21.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
