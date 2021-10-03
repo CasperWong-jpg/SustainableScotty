19049b08ba23188a1514fd161ee679e0c31e3ca3
CasperWong-jpg <casper.wong.001@hotmail.com>
2021-10-03 01:11:46 -0400

    Added Firebase :)

---
+++ Pods/Target Support Files/Pods-SustainableScotty-SustainableScottyUITests/Pods-SustainableScotty-SustainableScottyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SustainableScotty_SustainableScottyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SustainableScotty_SustainableScottyUITests
+@end
