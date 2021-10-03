5804799e4e580ae2166da1a4c97cf1b1f971797f
Sophia Lau <slau2022@gmail.com>
2021-10-03 17:37:52 -0400

    Added colors, images, and helper functions file. Start view is complete

--- SustainableScotty/ViewControllers/StartUpViewControllers/StartViewController.swift
+++ SustainableScotty/ViewControllers/StartUpViewControllers/StartViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class StartViewController: UIViewController {
 
+    @IBOutlet weak var SignUpButton: UIButton!
+    @IBOutlet weak var LogInButton: UIButton!
+   
     override func viewDidLoad() {
+        SignUpButton.circleCorner()
+        LogInButton.circleCorner()
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
