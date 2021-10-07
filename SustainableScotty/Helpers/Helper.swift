7c995cea739dd4fd9b9d8b2110b0f9f3cd6c2553
Sophia Lau <slau2022@gmail.com>
2021-10-07 17:28:34 -0400

    Established team and added some styling

--- SustainableScotty/Helpers/Helper.swift
+++ SustainableScotty/Helpers/Helper.swift
@@ -10,37 +10,4 @@ import Foundation
 class Helper{
     // place any helper functions here
     
-    extension UITextField {
-        func circleCorner () {
-            borderStyle = .none
-            
-            layer.cornerRadius = frame.size.height/2
-            
-            layer.borderWidth = .25
-            layer.borderColor= UIColor.white.cgColor
-            
-            layer.shadowOpacity= 1
-            layer.shadowRadius = 3
-            layer.shadowOffset = CGSize.zero
-            layer.shadowColor = UIColor.gray.cgColor
-            
-            let paddingView: UIView = UIView9frame: CGRect(x:0, y:0, width: 20, height: frame.height)
-            leftView = paddingView
-            leftViewMode = UITextField.ViewMode.always
-        }
-    }
-    
-    extension UIButton {
-        func circleCorner(){
-            layer.cornerRadius = frame.size.height / 2
-            
-            layer.borderWidth = .25
-            layer.borderColor= UIColor.white.cgColor
-            
-            layer.shadowOpacity= 1
-            layer.shadowRadius = 3
-            layer.shadowOffset = CGSize.zero
-            layer.shadowColor = UIColor.gray.cgColor
-        }
-    }
 }
