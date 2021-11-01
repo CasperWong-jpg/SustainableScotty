986fa4467ecc8bfd8bdc4455dfdebd14c1aacc13
CasperWong-jpg <casper.wong.001@hotmail.com>
2021-10-31 20:42:01 -0400

    Fixed accounts / sign in issues

---
+++ SustainableScotty/ViewControllers/CommunitiesViewControllers/Leaderboard.swift
@@ -0,0 +1,29 @@
+//
+//  Leaderboard.swift
+//  SustainableScotty
+//
+//  Created by Casper Wong on 10/31/21.
+//
+
+import UIKit
+
+class Leaderboard: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
