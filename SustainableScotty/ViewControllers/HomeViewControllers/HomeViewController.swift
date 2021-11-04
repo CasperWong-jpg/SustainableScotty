aaffa7f8760b752723471e0b8b063f0f4ca30f49
njzhu <njzhu@andrew.cmu.edu>
2021-11-04 18:55:19 -0400

    action stuff

--- SustainableScotty/ViewControllers/HomeViewControllers/HomeViewController.swift
+++ SustainableScotty/ViewControllers/HomeViewControllers/HomeViewController.swift
@@ -10,20 +10,29 @@ import Firebase
 
 class HomeViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var Header: UINavigationItem!
+    
+    @IBOutlet weak var actionCell: UITableViewCell!
     var actions: [Action] = [
-        Action(action_title: <#T##String#>, times_logged: <#T##Int#>, coins: <#T##Int#>)
+        Action(action_title: "Refill Reusable Waterbottle", times_logged: 125, coins: 15),
+        Action(action_title: "Brought Reusable Utensils", times_logged: 90, coins: 20),
+        Action(action_title: "Brought Reusable Mug", times_logged: 87, coins: 15)
     ]
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         Helper.createNavBar(navigationItem)
+        Header.title = "Your Frequent Actions"
 
         // Do any additional setup after loading the view.
     }
     
-
+    //extension HomeViewController: UITableViewDataSource {
+        
+    //}
     /*
     // MARK: - Navigation
 
