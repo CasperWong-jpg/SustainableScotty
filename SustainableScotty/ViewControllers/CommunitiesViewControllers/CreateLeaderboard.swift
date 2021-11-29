2435aa855215ea4e9d4729e92b5773e9703a65da
CasperWong-jpg <casper.wong.001@hotmail.com>
2021-11-29 11:59:29 -0500

    Create group :)

--- SustainableScotty/ViewControllers/CommunitiesViewControllers/CreateLeaderboard.swift
+++ SustainableScotty/ViewControllers/CommunitiesViewControllers/CreateLeaderboard.swift
@@ -6,24 +6,42 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateLeaderboard: UIViewController {
 
+    @IBOutlet weak var CommunityName: UITextField!
+    @IBOutlet weak var CreateButton: UIButton!
+    let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        CreateButton.circleCorner()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func ButtonPressed(_ sender: Any) {
+        if let name = CommunityName.text, name != "", let groupCreator = Auth.auth().currentUser?.email {
+            //var ref: DocumentReference? = nil
+            db.collection("communities").document(name).setData([
+                "CommunityName": name,
+                "Members": [groupCreator],
+                "Admins": [groupCreator]
+            ]) { err in
+                if let err = err {
+                    print("Error adding document: \(err)")
+                } else {
+                    print("Document added!")
+                    self.navigationController?.popViewController(animated: true)
+                }
+            }
+        }
+        else {
+            print("Error: either group name cannot be empty, or you are not logged in")
+        }
     }
-    */
-
+    
 }
